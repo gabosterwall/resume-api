a9ec77bd221d2eef141878c16d2e5f2635c03638
gabosterwall <gabosterwall@gmail.com>
2023-11-11 23:46:48 +0100

    Initial rest api for my resume

--- backend/controllers/resumeController.js
+++ backend/controllers/resumeController.js
@@ -1,31 +1,62 @@
 const asyncHandler = require('express-async-handler')
+const Resume = require('../models/resumeModel')
+
 
 // @desc    Get resume
 // @route   Get /api/resume
 // @access  Private
 const getResume = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get resume'})
+    const resume = await Resume.find()
+
+    res.status(200).json(resume)
 })
 
 // @desc    Set resume
 // @route   Set /api/resume
 // @access  Private
 const setResume =  asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Set resume'})
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please fill the text field.')
+    }
+
+    const resume = await Resume.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(resume)
 })
 
 // @desc    Update resume
 // @route   Pet /api/resume
 // @access  Private
 const updateResume =  asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update resume ${req.params.id} `})
+    const resume = await Resume.findById(req.params.id)
+
+    if(!resume){
+        res.status(400)
+        throw new Error('Resume not found.')
+    }
+
+    const updatedResume = await Resume.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedResume)
 })
 
 // @desc    Delete resume
 // @route   Delete /api/resume
 // @access  Private
 const deleteResume =  asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete resume ${req.params.id} `})
+    const resume = await Resume.findById(req.params.id)
+
+    if(!resume){
+        res.status(400)
+        throw new Error('Resume not found.')
+    }
+
+    await Resume.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('api/resume', require('./routes/resumeRoutes'))
+app.use('/api/resume', require('./routes/resumeRoutes'))
 
 app.use(errorHandler)
 
